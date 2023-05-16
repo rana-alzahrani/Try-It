3c3668c49811d417f97e8a96222b0aac707ffced
Ruba <roba8330@gmail.com>
2023-05-16 16:03:43 +0300

    Initial Commit

---
+++ Try It/Try_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Try_ItApp.swift
+//  Try It
+//
+//  Created by Ruba Abuhatlah on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Try_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
